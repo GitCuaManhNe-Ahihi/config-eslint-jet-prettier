2b660a478c84310e76958687b7318e9e0c5ea963
GitCuaManhNe-Ahihi <shuriken8668@gmail.com>
2022-07-18 17:27:16 +0700

    done

---
+++ src/math/add.test.ts
@@ -0,0 +1,6 @@
+import add from '@src/math/add';
+    describe('this is a test', () => {
+    it('should pass', () => {
+        expect(add(1, 3)).toBe(4);
+    });
+    });
---
+++ src/math/add.ts
@@ -0,0 +1,3 @@
+const add = (a: number, b: number) => a + b;
+console.log('string');
+export default add;
